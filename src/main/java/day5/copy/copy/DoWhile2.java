f10c74fadaf7e40acec5ee29df963457af5c89ea
Chandrakant <chandrakantjadhav.qa@gmail.com>
2026-03-27 09:45:49 +0530

    copy files

---
+++ src/main/java/day5/copy/copy/DoWhile2.java
@@ -0,0 +1,39 @@
+package day5.copy.copy;
+
+public class DoWhile2 {
+	public static void main(String[] args) {
+	
+	    testMenthod();
+		
+		int i=1;
+		
+		do
+		{
+			System.out.println(i);
+			i++; 
+		}
+		while (i<=10);
+	}
+	
+	
+	
+
+
+
+public static void testMenthod()
+    {
+	
+int s=10;
+	
+	do
+	{
+		System.out.println(s);
+		s--; 
+	}
+	while (s>=1);
+}
+
+	
+	}
+
+
---
+++ src/main/java/day5/copy/copy/ForLoopDemo.java
@@ -0,0 +1,13 @@
+package day5.copy.copy;
+
+public class ForLoopDemo {
+
+	public static void main(String[] args) {
+		for (int i=1; i<=10; i++)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
